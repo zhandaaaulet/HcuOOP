05fac33db618af6df6e70f2af34096cb39134ae3
zhandaaaulet <zh.myrzatayev@gmail.com>
2020-12-27 18:06:33 +0600

    second commit

--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -1,5 +1,48 @@
 package com.company;
 
 public class Date {
-    
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year) {
+        setDay(day);
+        setMonth(month);
+        setYear(year);
+    }
+
+    @Override
+    public String toString() {
+        return day + ":" + month + ":" + year;
+    }
 }
